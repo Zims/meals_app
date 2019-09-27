c47f77f7a69f870fa8118dffd7abf5357e0a6f18
Zims <zims@inbox.lv>
2019-09-27 21:06:45 +0300

    Adding a "Mark as Favorite" Feature

--- lib/main.dart
+++ lib/main.dart
@@ -22,13 +22,14 @@ class _MyAppState extends State<MyApp> {
     'vegetarian': false,
   };
 
-  List<Meal> _avilableMeals = DUMMY_MEALS;
+  List<Meal> _availableMeals = DUMMY_MEALS;
+  List<Meal> _favoriteMeals = [];
 
   void _setFilters(Map<String, bool> filterData) {
     setState(() {
       _filters = filterData;
 
-      _avilableMeals = DUMMY_MEALS.where((meal) {
+      _availableMeals = DUMMY_MEALS.where((meal) {
         // if(_filters['gluten'] == true) =
         if (_filters['gluten'] && !meal.isGlutenFree) {
           return false;
@@ -46,6 +47,27 @@ class _MyAppState extends State<MyApp> {
         //chek whole dummy list for ! gluten free. If it isn`t than it`s excluded because the filter is on
       }).toList();
     });
+  }
+
+  void _toggleFavorite(String mealId) {
+    final existingIndex =
+        _favoriteMeals.indexWhere((meal) => meal.id == mealId);
+
+    if (existingIndex >= 0) {
+      setState(() {
+        _favoriteMeals.removeAt(existingIndex);
+      });
+    } else {
+      setState(() {
+        _favoriteMeals.add(
+          DUMMY_MEALS.firstWhere((meal) => meal.id == mealId),
+        );
+      });
+    }
+  }
+
+  bool _isMealFavorite(String id) {
+    return _favoriteMeals.any((meal) => meal.id == id);
   }
 
   @override
@@ -72,10 +94,10 @@ class _MyAppState extends State<MyApp> {
       ),
       // home: CategoriesScreen(),
       routes: {
-        '/': (ctx) => TabsScreen(),
+        '/': (ctx) => TabsScreen(_favoriteMeals),
         CategoryMealsScreen.routeName: (ctx) =>
-            CategoryMealsScreen(_avilableMeals),
-        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+            CategoryMealsScreen(_availableMeals),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(_toggleFavorite, _isMealFavorite),
         FiltersScreen.routeName: (ctx) => FiltersScreen(_filters, _setFilters)
       },
     );
